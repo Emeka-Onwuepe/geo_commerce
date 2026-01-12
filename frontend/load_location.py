bebced5714de1362654258d59ebe7e10f9291882
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2026-01-12 19:25:00 +0100

    debugging

--- frontend/load_location.py
+++ frontend/load_location.py
@@ -10,17 +10,18 @@ cluster_file = f'{base_file}/cluster_details.csv'
 
 
 def get_outlets(lat,lon):
-    cluster_model = pickle.load(open(model_file, 'rb'))
-    df = pd.read_csv(cluster_file)
+    # cluster_model = pickle.load(open(model_file, 'rb'))
+    # df = pd.read_csv(cluster_file)
         
-    lat = float(lat)
-    lon = float(lon)
-    geo_data = np.deg2rad([lat, lon]).reshape(-1, 2)
-    pre_df = pd.DataFrame(data=geo_data,
-                          columns= ['latitude_rad', 'longitude_rad']
-            )
+    # lat = float(lat)
+    # lon = float(lon)
+    # geo_data = np.deg2rad([lat, lon]).reshape(-1, 2)
+    # pre_df = pd.DataFrame(data=geo_data,
+    #                       columns= ['latitude_rad', 'longitude_rad']
+    #         )
 
-    [cluster_label] = cluster_model.predict(pre_df)
-    reg = df[df['cluster_label']==cluster_label]['registration_area'].unique()
-    return reg
+    # [cluster_label] = cluster_model.predict(pre_df)
+    # reg = df[df['cluster_label']==cluster_label]['registration_area'].unique()
+    # return reg
+    return ['emeka','onwuepe']
     
